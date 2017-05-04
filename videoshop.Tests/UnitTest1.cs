c7986e5d55cb182c5d736b32ef346bc3b6d38ac4
Administrator <Administrator@SKY-20160625DYS>
2017-05-04 23:27:04 +0800

    添加项目文件。

---
+++ videoshop.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace videoshop.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
