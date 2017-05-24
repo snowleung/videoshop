0dc3551f2fdde37edd716088f46aff75ee394cd1
sn <sn@home>
2017-05-24 23:13:14 +0800

    add webconfig and ignore db file

--- videoshop/admin/users.aspx.cs
+++ videoshop/admin/users.aspx.cs
@@ -4,13 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Configuration;
+
 
 namespace videoshop.admin
 {
     public partial class users : System.Web.UI.Page
     {
+        protected void Bind_DataSource() {
+            SqlDataSource1.ConnectionString = ConfigurationManager.ConnectionStrings["videoshop"].ConnectionString;
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
+            Bind_DataSource();
             string nickName = Request.QueryString["nickname"];
             if (nickName != null) {
                 SqlDataSource1.SelectCommand = "select * from users where nickname='" + nickName + "'";
